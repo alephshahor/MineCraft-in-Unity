b74980ae7197fa28ffcc0c472b4b05fffe9276c5
alephshahor <adrianalvarezleon@gmail.com>
2018-02-01 21:51:21 +0000

    Third Commit

---
+++ AirBlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirBlock : Block {
+
+	public AirBlock(Vector3 posicion) : base (posicion){}
+
+	public override Tile getTile (Direction direction){
+		Tile tile = new Tile();
+		switch(direction){
+
+			default:
+					tile.positionX = 3;
+					tile.positionY = 3;
+					break;
+
+		}
+
+			return tile;
+	}
+}
---
+++ GrassBlock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassBlock : Block {
+
+		public GrassBlock(Vector3 posicion) : base (posicion){}
+		public GrassBlock() : base (){}
+
+		public override Tile getTile (Direction direction){
+			Tile tile = new Tile();
+			switch(direction){
+
+				case Direction.Up:
+						 tile.positionX = 2;
+						 tile.positionY = 0;
+						 break;
+
+			  case Direction.Down:
+						tile.positionX = 1;
+						tile.positionY = 0;
+						break;
+
+				default:
+						tile.positionX = 3;
+						tile.positionY = 0;
+						break;
+
+			}
+
+				return tile;
+		}
+
+}
